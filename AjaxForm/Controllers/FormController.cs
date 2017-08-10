f554a07683f60ca5c1fe2eefc750b865c721dcf9
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-10 19:39:32 -0400

    created form model. created form controller and api with model data. Added main.js and added to bundleconfig, tested. API working through postman. loadForms function not running on site load.

---
+++ AjaxForm/Controllers/FormController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AjaxForm.Models;
+
+namespace AjaxForm.Controllers
+{
+    public class FormController : ApiController
+    {
+        public static List<FormModel> listOfForms = new List<FormModel>
+        {
+            new FormModel{ Id = 1,
+                FirstName = "Jeff",
+                LastName = "Longo",
+                Phone = "4077587623",
+                Cell = true,
+                EMail = "jeffreylongo@yahoo.com",
+                OptOut = true },
+        };
+
+        public IHttpActionResult Get()
+        {
+            return Ok(listOfForms);
+        }
+    }
+}
---
+++ AjaxForm/Scripts/main.js
@@ -0,0 +1,19 @@
+﻿console.log("loaded main.js");
+
+let loadForms = () => {
+    $.ajax({
+        url: "/api/form",
+        dataType: "json",
+        success: (listOfForms) => {
+            listOfForms.map((m) => {
+                var _li = $("<li>").html(m.Form);
+                $("#formData").append(_li);
+            });
+        }
+    });
+}
+
+$(document).ready(() => {
+    console.log("ready...");
+    loadForms();
+});
