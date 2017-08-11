deacee984a829e88f9cd06c7e08c88d223b4e05a
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-11 08:54:24 -0400

    added Id to all form inputs. added function to addToList and sendForm. Seems to be some syntax errors within js. Need to debug.

--- AjaxForm/Scripts/main.js
+++ AjaxForm/Scripts/main.js
@@ -16,6 +16,43 @@ let loadForms = () => {
     });
 };
 
+//add new data to list of form data
+let addToList = (data) => {
+    $("#formData")
+        .append($("<li>").html(data.FirstName + " " + data.LastName + " " + data.Phone + " " + data.Cell + " " + data.EMail + " " + data.OptOut));
+}
+
+//take data from form and send through api to ajax as new form data
+let sendForm = () => {
+
+    let newForm = {
+        firstName: $("#firstName").val(),
+        lastName: $("#lastName").val(),
+        email: $("#email").val(),
+        phone: $("#phone").val(),
+        cell: $("#cell").val(),
+        optOut: $("#optOut").val()
+    };
+
+    $.ajax({
+        url: "/api/form",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(newForm),
+        type: "POST",
+        success: (data) = {
+            addToList(data);
+        },
+        error: (data) => {
+            console.log("oops", data)
+        },
+        complete: (data) => {
+            console.log("done", data);
+        }
+
+    });
+}
+
 //make sure document is fully loaded before running functions for ajax call to api
 $(document).ready(() => {
 
