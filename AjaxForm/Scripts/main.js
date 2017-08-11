1225d27312a21fbf7547692e38444207e40505d0
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-10 20:38:36 -0400

    failsafe commit

--- AjaxForm/Scripts/main.js
+++ AjaxForm/Scripts/main.js
@@ -1,19 +1,23 @@
 ﻿console.log("loaded main.js");
 
+
 let loadForms = () => {
     $.ajax({
         url: "/api/form",
         dataType: "json",
         success: (listOfForms) => {
-            listOfForms.map((m) => {
-                var _li = $("<li>").html(m.Form);
-                $("#formData").append(_li);
+            listOfForms.map((l) => {
+                $("#formData")
+                    .append($("<li>").html(listOfForms))
             });
+
+
         }
     });
-}
+};
 
 $(document).ready(() => {
+
     console.log("ready...");
     loadForms();
 });
