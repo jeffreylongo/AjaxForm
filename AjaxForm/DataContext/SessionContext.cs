38901a7b6b840834096de11a1fa9347fb0bd3887
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-11 20:14:33 -0400

    added UserSessionDataModelsController. Still not getting session data back to view.

---
+++ AjaxForm/DataContext/SessionContext.cs
@@ -0,0 +1,14 @@
+﻿using AjaxForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AjaxForm.DataContext
+{
+    public class SessionContext :DbContext
+    {
+        public DbSet<UserSessionDataModel> Forms { get; set; }
+    }
+}
